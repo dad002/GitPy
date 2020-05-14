ac4f9048d13dd94911bc581af885a02302088711
Artemii Ryabov <a89056602510@bk.ru>
2020-05-14 12:04:55 +0300

    Subtraction added

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def mySum(a, b):
 	return a + b
 
+def mySub(a, b):
+	return a - b
+
 first = int(input("Enter first number"))
 second = int(input("Enter second number"))
+
+print(mySub(first, second))
